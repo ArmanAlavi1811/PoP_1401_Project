055332b6f5a62d9f7c18c77080e85b2784e07c38
ArmanAlavi1811 <arman.alavi1811@gmail.com>
2022-07-02 10:03:14 +0430

    Add project header

---
+++ project.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#define Loop_LT(i, f, n) for(i = f; i < n; i++)
+#define loop_LTE(i, f, n) for(i = f; i <= n; i++)
+
+#define Forever while(1)
+
+// ________________________
+// |                      |
+// |                      |
+// |                      |
+// |______________________|

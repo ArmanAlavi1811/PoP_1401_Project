5c28a4542338e11dd0e23e7892d3259d596aba8b
ArmanAlavi1811 <Arman.Alavi1811@gmail.com>
2022-07-03 14:10:42 +0100

    Install ncurses

--- project.h
+++ project.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <ncurses.h>
 
 #define Loop_LT(i, f, n) for(i = f; i < n; i++)
 #define loop_LTE(i, f, n) for(i = f; i <= n; i++)

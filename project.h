a1643a5b21a250544f07e18505433fc69d068760
ArmanAlavi1811 <arman.alavi1811@gmail.com>
2022-07-04 15:24:39 +0430

    write basic functions

--- project.h
+++ project.h
@@ -2,15 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#include <ncurses.h>
+
+#define elif else if
 
 #define Loop_LT(i, f, n) for(i = f; i < n; i++)
 #define loop_LTE(i, f, n) for(i = f; i <= n; i++)
 
 #define Forever while(1)
-
-// ________________________
-// |                      |
-// |                      |
-// |                      |
-// |______________________|
